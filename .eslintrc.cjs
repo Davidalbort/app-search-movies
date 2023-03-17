2f98f61078de856bc56de3bc60a93b08a74effab
Roger David Alba Ortgea <rogerdavidalbaortega@gmail.com>
2023-03-17 11:08:29 -0500

    Add husky and eslint-stage for keep code equal before and after to make commit

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,46 +1,33 @@
 const RULES = {
 	OFF: "off",
 	WARN: "warn",
-	ERROR: "error"
+	ERROR: "error",
 }
 module.exports = {
-	"env": {
-		"browser": true,
-		"es2021": true,
-		"node": true
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
 	},
-	"extends": [
+	extends: [
 		"eslint:recommended",
 		"plugin:react/recommended",
 		"plugin:@typescript-eslint/recommended",
-		"prettier"
+		"prettier",
 	],
-	"overrides": [
-	],
-	"parser": "@typescript-eslint/parser",
-	"parserOptions": {
-		"ecmaVersion": "latest",
-		"sourceType": "module"
+	overrides: [],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
 	},
-	"plugins": [
-		"react",
-		"@typescript-eslint"
-	],
-	"rules": {
-		"indent": [
-			"error",
-			"tab"
-		],
-		"quotes": [
-			"error",
-			"double"
-		],
-		"semi": [
-			"error",
-			"never"
-		],
+	plugins: ["react", "@typescript-eslint"],
+	rules: {
+		indent: ["error", "tab"],
+		quotes: ["error", "double"],
+		semi: ["error", "never"],
 		"react/react-in-jsx-scope": RULES.OFF,
-		"max-len": [RULES.ERROR,{code: 100}],
-		"no-empty": RULES.WARN
-	}
+		"max-len": [RULES.ERROR, { code: 100 }, { tabWidth: 1 }],
+		"no-empty": RULES.WARN,
+	},
 }
