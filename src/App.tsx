d4e6f186dd0f46e1642019be3d8862336088f806
Roger David Alba Ortgea <rogerdavidalbaortega@gmail.com>
2023-03-21 18:26:35 -0500

    Feature: Add logic app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,54 @@
-export const App = () => {
+import { useState } from "react"
+import { Form } from "./components/form/Form"
+import { Movies } from "./components/movies/Movies"
+import { useSearch } from "./hooks/useSearch"
+import { SearchFromApi } from "./types/types.td"
+import { searchMovies } from "./utils/services/search/searchMovies"
+
+function useMovies() {
+	const [movies, setMovies] = useState<SearchFromApi[]>([])
+	const [loading, setLoading] = useState(false)
+	const [error, setError] = useState("")
+	const getMovies = async (search: string) => {
+		try {
+			setLoading(true)
+			const newMovie = await searchMovies(search)
+			setMovies(newMovie)
+		} catch (error) {
+			setError(`${error}message`)
+		} finally {
+			setLoading(false)
+		}
+	}
+
+	return { movies, loading, getMovies, error }
+}
+
+export function App() {
+	const { search, message, handleChange, updateMessage } = useSearch()
+	const { movies, getMovies, error } = useMovies()
+	const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault()
+		if (!search) {
+			updateMessage("Can't search movies with empty fields")
+		}
+		getMovies(search)
+	}
 	return (
-		<>
-			<h1>Search Films</h1>
-		</>
+		<div className="page">
+			<header>
+				<h1>Search Films</h1>
+				<Form
+					search={search}
+					message={message}
+					handleChange={handleChange}
+					handleSubmit={handleSubmit}
+				/>
+			</header>
+			<main>
+				<Movies results={movies} />
+			</main>
+			{error && <span>{error}</span>}
+		</div>
 	)
 }
