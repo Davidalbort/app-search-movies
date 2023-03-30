6ed1ff6aa7adf4e31056923278e6f2f22f841017
Roger David Alba Ortgea <rogerdavidalbaortega@gmail.com>
2023-03-30 18:11:20 -0500

    Reafactor: move lastSearch to useMovie for more efficent

--- src/hooks/useMovies.ts
+++ src/hooks/useMovies.ts
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
 import { Movie } from "../types/types.td"
 import { searchMovies } from "../utils/services/search/searchMovies"
 
@@ -6,9 +6,12 @@ export function useMovies() {
 	const [movies, setMovies] = useState<Movie[]>()
 	const [loading, setLoading] = useState(false)
 	const [error, setError] = useState("")
+	const lastSearch = useRef("")
 	const getMovies = async (search: string) => {
+		if (lastSearch.current === search) return
 		try {
 			setLoading(true)
+			lastSearch.current = search
 			const newMovie = await searchMovies(search)
 			setMovies(newMovie)
 		} catch (error) {
